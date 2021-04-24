428da9907c09884e83ada9fdef9de5c41c125a62
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-24 04:16:10 -0500

    commit tree 4bb7a1b6e27ca7490150a265e9549448fd66602d

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,47 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react'
+
+export default class YouTubeDebugger extends Component {
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    bitrateClickHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+
+    resolutionClickHandler = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: '720p'
+                }
+            }
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.bitrateClickHandler} className="bitrate"></button>
+                <button onClick={this.resolutionClickHandler} className="resolution"></button>
+            </div>
+        )
+    }
+}
